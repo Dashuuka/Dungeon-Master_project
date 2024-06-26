8a0f06e195f7d470421d3da7315a47396f2c8925
M3wory <orand9@yandex.ru>
2024-06-26 17:05:10 +0300

    Edited structure

---
+++ Assets/Scripts/GenerationScripts/DungeonGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour{
+    public GameObject floor;
+
+    void Start(){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                Instantiate(floor, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+    }
+
+
+    void Update(){
+        
+    }
+}
