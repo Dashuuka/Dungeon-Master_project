f1ad4c38561ca5e410d41f5b345410e10b925b4e
Alex Sytsin <sytin2006@gmail.com>
2024-06-27 17:14:28 +0300

    Animation and Movement

--- Assets/Scripts/PlayerScripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviourScript.cs
@@ -4,16 +4,22 @@ public class PlayerBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float speed = 7;
     private Rigidbody2D body;
+    private Animator anim;
+
+
 
     private void Awake()
     {
+       // Take properties from player 
        body = GetComponent<Rigidbody2D>();
+       anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity =new Vector2(horizontalInput*speed, Input.GetAxis("Vertical") * speed);
+        float verticalInput = Input.GetAxis("Vertical");
+        body.velocity =new Vector2(horizontalInput*speed, verticalInput * speed);
 
         //Flip Player Facing Left Right
         if (horizontalInput > 0)
@@ -24,5 +30,8 @@ public class PlayerBehaviourScript : MonoBehaviour
         {
             transform.localScale = new Vector3(-1,1,1); 
         }
+
+
+        anim.SetBool("Run",horizontalInput!=0 || verticalInput!=0);
     }
 }
