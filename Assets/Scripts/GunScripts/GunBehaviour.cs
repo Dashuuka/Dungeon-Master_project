d3aaf265b139e4813e8cc75d36a42fd94bbb3db6
Darya Sidorskaya <darya.sidroskaya139@gmail.com>
2024-06-28 13:47:31 +0300

    Update PlayerBehaviourScript.cs and GunBehaviour.cs

--- Assets/Scripts/GunScripts/GunBehaviour.cs
+++ Assets/Scripts/GunScripts/GunBehaviour.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GunBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectilePrefab; // Префаб снаряда
+    public Transform firePoint; // Точка, из которой вылетает снаряд
+    public float projectileSpeed = 10f; // Скорость снаряда (будет прокачиваться с повышением уровня)
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RotateGunTowardsMouse();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void RotateGunTowardsMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        Vector3 direction = mousePosition - transform.position;
+        direction.Normalize();
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    private void Shoot()
+    {
+
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+
+        Vector3 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position).normalized;
+
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+
+        /* // Воспроизводим анимацию атаки
+         anim.SetTrigger("Attack");*/
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerBehaviourScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerBehaviourScript.cs
@@ -12,9 +12,9 @@ public class PlayerBehaviourScript : MonoBehaviour
 
     private void Awake()
     {
-       // Take properties from player 
-       body = GetComponent<Rigidbody2D>();
-       anim = GetComponent<Animator>();
+        // Take properties from player 
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -24,16 +24,42 @@ public class PlayerBehaviourScript : MonoBehaviour
         body.velocity = movement.normalized * speed;
 
         //Flip Player Facing Left Right
-        if (movement.x > 0)
+        /*if (movement.x > 0)
         {
             transform.localScale = Vector3.one;
         }
         else if (movement.x < 0)
         {
             transform.localScale = new Vector3(-1,1,1); 
-        }
+        }*/
+
+        RotatePlayerTowardsMouse();
 
 
         anim.SetBool("Run", movement.magnitude != 0);
     }
+
+    private void RotatePlayerTowardsMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+
+        Vector3 direction = mousePosition - transform.position;
+        direction.Normalize();
+
+        if (direction.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public bool canAttack()
+    {
+        return body.velocity.magnitude > 0;
+    }
 }
+
