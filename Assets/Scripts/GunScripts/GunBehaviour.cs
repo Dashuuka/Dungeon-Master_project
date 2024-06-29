ee81a193ec5e8544b29aef9e75f57f9b4f1393bc
M3wory <orand9@yandex.ru>
2024-06-29 10:15:33 +0300

    Fixed bugs and resolved conflict

--- Assets/Scripts/GunScripts/GunBehaviour.cs
+++ Assets/Scripts/GunScripts/GunBehaviour.cs
@@ -2,75 +2,52 @@ using UnityEngine;
 
 public class GunBehaviour : MonoBehaviour
 {
-    public GameObject bulletPrefab; // Префаб пули
-    public Transform firePoint; // Точка вылета пули
-    public float projectileSpeed; // Скорость пули
-    public Animator anim; // Аниматор оружия
-    public float fireRate=1f; // Скорость стрельбы (выстрелов в секунду)
-    private float nextFireTime; // Время следующего выстрела
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float projectileSpeed;
+    public Animator anim;
+    public float fireRate = 1f;
+    private float nextFireTime;
 
-    // Метод для стрельбы
     public void Shoot()
     {
-        // Проверяем, можно ли стрелять
+
         if (Time.time > nextFireTime)
         {
-            // Создаем пулю
+
             firePoint.rotation = Quaternion.Euler(firePoint.rotation.eulerAngles + new Vector3(0, 0, 270));
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
-            // Получаем направление к курсору
             Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - firePoint.position;
-            direction.z = 0; // Убираем z-координату, чтобы пуля летела в 2D плоскости
+            direction.z = 0;
 
-            // Присваиваем пуле скорость
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
                 rb.velocity = direction.normalized * projectileSpeed;
             }
 
-
-            // Обновляем время следующего выстрела
             nextFireTime = Time.time + 1f / fireRate;
-        }
-    }
-
-    // Метод, вызываемый при завершении анимации стрельбы
-    private void AnimationEvent_ShootFinished()
-    {
-        // Удаляем пулю
-        GameObject bullet = transform.GetChild(0).gameObject;
-        if (bullet != null)
-        {
-            Destroy(bullet);
         }
     }
 
     private void RotateGunTowardsMouse()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = transform.position.z; // Устанавливаем z-координату мыши на z-координату объекта "gun"
+        mousePosition.z = transform.position.z;
 
+        Vector3 direction = mousePosition - transform.parent.position;
 
-        // Вычисляем вектор направления от объекта "gun" к мыши
-        Vector3 direction = mousePosition - transform.position;
-
-        // Вычисляем угол поворота в градусах
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        // Инвертируем угол, если лук направлен влево ИЛИ вверх
-        if (direction.x < 0.01f)
+        if (direction.x < 0f)
         {
             angle += 180f;
         }
-
-        // Поворачиваем объект "gun"
-        transform.rotation = Quaternion.Euler(0f, 0f, angle); // Поворачиваем только по оси Z
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
 
-    // Обновление
     void Update()
     {
         RotateGunTowardsMouse();
